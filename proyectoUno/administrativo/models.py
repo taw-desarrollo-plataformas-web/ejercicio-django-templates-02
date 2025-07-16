3a017727a5d71e1a3a2cc285f8f95ec57f3ed6c6
René Elizalde <reroes799@gmail.com>
2025-07-16 16:10:30 -0500

    ejemplos de uso de templates

---
+++ proyectoUno/administrativo/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+# Create your models here.
+
+class Estudiante(models.Model):
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    cedula = models.CharField(max_length=30, unique=True)
+    correo = models.EmailField()
+
+    def __str__(self):
+        return "%s %s %s %s" % (self.nombre,
+                self.apellido,
+                self.cedula,
+                self.correo)
+
+    def get_provincia(self):
+        """
+        """
+        dato = self.cedula[0:2]
+        valor = "Sin Provincia"
+        if dato == "11":
+            valor = "Loja"
+        else:
+            if dato == "17":
+                valor = "Pichincha"
+        return valor
+
+class NumeroTelefonico(models.Model):
+    telefono = models.CharField(max_length=100)
+    tipo = models.CharField(max_length=100)
+    estudiante = models.ForeignKey(Estudiante, on_delete=models.CASCADE,
+            related_name="numeros_telefonicos")
+
+    def __str__(self):
+        return "%s %s" % (self.telefono, self.tipo)
